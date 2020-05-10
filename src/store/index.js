c35efa4a718606c1166d08f01b7c3624e8c46c79
Vsevolod Skripnik <vsevolod_skripnik@protonmail.com>
2020-05-10 20:32:58 +0300

    Counter app

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,15 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    count: 0,
   },
   mutations: {
+    INCREMENT_COUNT: (state, value) => {
+      state.count += value;
+    },
+    DECREMENT_COUNT: (state, value) => {
+      state.count -= value;
+    },
   },
   actions: {
   },
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,26 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{ count }}</h1>
+    <button @click="decrement_count">-</button>
+    <button @click="increment_count">+</button>
   </div>
 </template>
+
+<script>
+
+import { mapState } from 'vuex';
+
+export default {
+  computed: {
+    ...mapState(['count']),
+  },
+  methods: {
+    increment_count() {
+      this.$store.commit('INCREMENT_COUNT', 2);
+    },
+    decrement_count() {
+      this.$store.commit('DECREMENT_COUNT', 2);
+    },
+  },
+};
+</script>
